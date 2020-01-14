41c57bdc936523d04db3bf9dbc84680a8d19a34f
Joris ter Wolbeek <21074092@talnet.nl>
2020-01-14 11:51:43 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,5 @@
+<?php
+$voornaam=readline("wat is je voornaam?".PHP_EOL);
+$achternaam=readline("en wat is je achter naam?".PHP_EOL);
+$voornaam_Achternaam=$voornaam." ".$achternaam;
+echo("jouw naam is: ".$voornaam_Achternaam.PHP_EOL);
